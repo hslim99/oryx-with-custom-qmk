6eb49a1bf9c30d4b941e63d9aac559c5e8257b2c
Hyunsoo Lim <hslim9909@gmail.com>
2025-03-01 23:49:26 +0900

    Initialize achordion

--- d5aEx/keymap.c
+++ d5aEx/keymap.c
@@ -1,8 +1,14 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "quantum.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
+
+void housekeeping_task_user(void) {
+  achordion_task();
+}
+
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
@@ -96,6 +102,8 @@ uint16_t get_combo_term(uint16_t combo_index, combo_t *combo) {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_achordion(keycode, record)) { return false; }
+
     if (keycode == RGB_SLD) {
         if (rawhid_state.rgb_control) {
             return false;
